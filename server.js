b4a2e526bd43aeb8bc792b3f2295dc1f7352dfb3
João Pedro <joaocarmolp@gmail.com>
2024-11-21 17:12:55 -0300

    Conectando ao banco e modularizando o código

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+import routes from "./src/routes/postRoutes.js";
+
+const app = express();
+routes(app)
+app.listen(3000, () => {
+	console.log("Servidor escutando...");
+});
+
+
