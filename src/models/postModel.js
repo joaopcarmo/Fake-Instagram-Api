cf3e8bafa4d386c73acdff2ac347b258bcf642fd
João Pedro <joaocarmolp@gmail.com>
2024-11-24 15:43:36 -0300

    implementando armazenamento e upload de imagens

--- src/models/postModel.js
+++
@@ -1,9 +0,0 @@
-import conectarAoBanco from "../config/dbConfig.js";
-
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
-
-export default async function getTodosPosts() {
-	const db = conexao.db("imersao-backend");
-	const colecao = db.collection("posts");
-	return colecao.find().toArray();
-}
