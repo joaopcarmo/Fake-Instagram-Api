317e943ebf76a568230b3b4a28c1be62323b076d
João Pedro <joaocarmolp@gmail.com>
2024-11-24 23:56:29 -0300

    integrando com front

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import conectarAoBanco from "./src/config/dbConfig.js";
 import routes from "./src/routes/postRoutes.js";
 
 const app = express();
+app.use(express.static("uploads"))
 routes(app)
 app.listen(3000, () => {
 	console.log("Servidor escutando...");
